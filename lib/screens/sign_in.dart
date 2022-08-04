c57524a3d7306dad6cecb20a0f5dc39cd0220663
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-04 11:13:39 +0600

    Food Order App: Sign In UI Added

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,10 +1,128 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/food_app_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    'Sign in to continue',
+                  ),
+                  Text(
+                    'Foodie',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.green.shade900,
+                          blurRadius: 5,
+                          offset: const Offset(3, 3),
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SignInButton(
+                        Buttons.Facebook,
+                        text: "Sign in with Facebook",
+                        onPressed: () {},
+                      ),
+                      !Platform.isIOS
+                          ? SignInButton(
+                              Buttons.Google,
+                              text: "Sign in with Google",
+                              onPressed: () {},
+                            )
+                          : Container(),
+                      !Platform.isAndroid
+                          ? SignInButton(
+                              Buttons.Apple,
+                              text: "Sign in with Apple",
+                              onPressed: () {},
+                            )
+                          : Container(),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'By sign in you are agree to our',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              debugPrint('Terms Button Clicked');
+                            },
+                            child: Text(
+                              'Terms',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            'and',
+                            style: TextStyle(
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          InkWell(
+                            onTap: () {
+                              debugPrint('Privacy Policy Button Clicked');
+                            },
+                            child: Text(
+                              'Privacy Policy',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
